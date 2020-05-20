29336373dc1e9eef63dab22c9560cc6d177b4446
unknown <rhehdrla@naver.com>
2020-05-20 16:15:14 +0900

    기본 기능 구현 및 storing 기능 구현

--- WebContent/App/app.js
+++ WebContent/App/app.js
@@ -4,11 +4,11 @@
 function get(id) { return document.getElementById(id) };
 function hide(id) { get(id).style.visibility = 'hidden' };
 function show(id) { get(id).style.visibility = 'visible' };
-function html(id) { get(id).innerHTML = html };
 function timeStamp() { var d = new Date(); return d.getTime() };
-function random(min, max) { return Math.round(min + Math.random() * (max - min)) }
+function random(min, max) { return Math.floor(min + Math.random() * (max + 1 - min)) }
 function randomChoice(choices) { return choices[random(0, choices.length - 1)] }
-
+// 0 - 6
+// 0 - 0.5 / 0.5 - 1.5 / 1.5-2.5 / 2.5 - 3.5 /... - 5.5/ 5.5 - 6
 
 window.requestAnimFrame = (function () {
 	return window.requestAnimationFrame ||
@@ -21,12 +21,14 @@ window.requestAnimFrame = (function () {
 
 
 // variables
-var KEY = { ESC: 27, SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 },
+var KEY = { ESC: 27, SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40, SHIFT: 16 },
 	DIR = { UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3, MIN: 0, MAX: 3, FULLDROP: 4 },
 	canvas = get('canvas'),
 	ctx = canvas.getContext('2d'),
 	ucanvas = get('upcoming'),
 	uctx = ucanvas.getContext('2d'),
+	scanvas = get('storing'),
+	sctx = scanvas.getContext('2d'),
 	speed = { start: 0.6, decrement: 0.01, min: 0.1 },
 	nx = 10, ny = 20, nu = 5;
 
@@ -36,19 +38,22 @@ var dx, dy, // 1blcok의 real pixel
 	blocks, // court
 	actions,
 	playing,
+	storing,
 	dt,
 	current,
 	next,
+	stored,
 	score,
 	vscore, // score 를 촤라락 표시하기위해 필요
 	rows,
+	vrows,
 	step;
+	
 
 
 
 
-// blocks
-
+// blocks , type
 var i = { size: 4, blocks: [0x0F00, 0x2222, 0x00F0, 0x4444], color: 'cyan' }; // 짝대기
 var j = { size: 3, blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20], color: 'blue' }; // J
 var l = { size: 3, blocks: [0x4460, 0x0E80, 0xC440, 0x2E00], color: 'orange' }; // L
@@ -63,9 +68,8 @@ function eachblock(type, x, y, dir, fn) {
 	// x+col, y+row
 	var count = 0;
 	for (var i = 0; i < 16; i++) {
-			// 0000 : false;
-			console.log('bit : ' + bit);
-		if ((cur & bit)) { 
+		// 0000 : false;
+		if ((cur & bit)) {
 			count++;
 			fn(x + col, y + row);
 		}
@@ -76,7 +80,6 @@ function eachblock(type, x, y, dir, fn) {
 		bit = bit >> 1;
 	}
 
-	console.log('block count : ' + count + ' (x,y) : ' + `(${x},${y})` );
 }
 
 function unoccupied(type, x, y, dir) {
@@ -92,21 +95,22 @@ function unoccupied(type, x, y, dir) {
 	return result;
 }
 
-function isValidRotation() {
-	// 좀따가
-
-	var result = true;
-}
-
-
-
-
 function randomPiece() {
 	// 이렇게 해도 될 것 같은데..!
 	var pieces = [i, j, l, o, s, t, z];
 
 	var blockType = randomChoice(pieces);
-	return { type: blockType, dir: DIR.UP, x: random(0, nx - 4), y: 0 }
+	var piece = { type: blockType, dir: DIR.UP, x: random(0, nx - 4), y: 0 };
+	randomPosition(piece);
+	return piece;
+}
+
+function randomPosition(piece) {
+	var count = 0;
+	while (!unoccupied(piece.type, piece.x, 0, piece.dir) && count < 10) {
+		piece.x = random(0, nx - 2);
+		count++;
+	}
 }
 
 
@@ -123,7 +127,7 @@ function run() {
 		draw();
 		last = now;
 
-		requestAnimationFrame(frame);
+		requestAnimationFrame(frame, canvas);
 	}
 
 	resize();
@@ -140,12 +144,12 @@ function keydown(ev) {
 	if (playing) {
 		var handled = false;
 		switch (ev.keyCode) {
-
 			case KEY.UP: actions.push(KEY.UP); handled = true; break;
 			case KEY.DOWN: actions.push(KEY.DOWN); handled = true; break;
 			case KEY.LEFT: actions.push(KEY.LEFT); handled = true; break;
 			case KEY.RIGHT: actions.push(KEY.RIGHT); handled = true; break;
-			case KEY.SPACE: actions.push(KEY.FULLDROP); handled = true; break;
+			case KEY.SPACE: actions.push(KEY.SPACE); handled = true; break;
+			case KEY.SHIFT: actions.push(KEY.SHIFT); handled = true; break;
 			case KEY.ESC: lose(); handled = true;
 		}
 	} else {
@@ -165,6 +169,9 @@ function resize() {
 	ucanvas.width = ucanvas.clientWidth;
 	ucanvas.height = ucanvas.clientHeight;
 
+	scanvas.width = scanvas.clientWidth;
+	scanvas.height = scanvas.clientHeight;
+
 	dx = canvas.width / nx;
 	dy = canvas.height / ny;
 }
@@ -178,39 +185,40 @@ function lose() { show('start'); setVisualScore(score); playing = false; }
 
 function setVisualScore(n) { vscore = n || score; invalidateScore(); }
 
-function setScore(n) { score = n; setVisualScore(); }
+function setScore(n) { score = n; }
 
 function addScore(n) { setScore(score + n); }
 
-function clearScore() { setScore(0); }
+function clearScore() { setScore(0); vscore = 0; invalidateScore(); }
 
-function clearRows() { setRows(0); }
+function clearRows() { setRows(0); vrows = 0; invalidateRows(); }
+
+function setVisualRows(n) { console.log('setVisualRows(N)'); vrows = n || rows; invalidateRows(); }
 
 function setRows(n) {
 	rows = n;
 	step = Math.max(speed.min, speed.start - rows * speed.decrement);
-	invalidateRows();
 }
 
 function addRows(n) { setRows(rows + n); }
 
 // Blocks에 현재 표시되는 블럭들을 놓는것은 내방식으로 구현한다.
 function getBlock(x, y) {
-			// 이부분은 정확히는 모르겠지만.. blocks 가 2차원배열로 선언된상태가 아닐 때 null로 보내주는거같다..
-	 return (blocks && blocks[x]) ? blocks[x][y] : null; 
+	// 이부분은 정확히는 모르겠지만.. blocks 가 2차원배열로 선언된상태가 아닐 때 null로 보내주는거같다..
+	return (blocks && blocks[x]) ? blocks[x][y] : null;
 }
 
 function setBlock(input) {
 
 	var x = input.x, y = input.y, dir = input.dir, type = input.type;
-
 	// 좌표 (x,y)에 블럭이 있으면 type을 넣어준다. , 아니면  null
 	eachblock(type, x, y, dir, function (x, y) {
-	
-		if (blocks[x][y] == null) blocks[x][y] = type;
+		//array가 2차원으로 선언을 해주지 않으면 사용할 수 없다!! blocks[x] 가 없으면 []로 선언해준다는 뜻
+		blocks[x] = blocks[x] || [];
+		blocks[x][y] = type;
 	})
-}
 
+}
 
 function clearBlocks() {
 	blocks = [];
@@ -223,6 +231,11 @@ function setCurrentPiece(piece) { current = piece || randomPiece(); invalidate()
 
 function setNextPiece(piece) { next = piece || randomPiece(); invalidateNext(); }
 
+function setStoredPiece(piece) {
+	stored = piece || randomPiece();
+	invalidateStored();
+}
+
 function reset() {
 	dt = 0;
 
@@ -233,17 +246,24 @@ function reset() {
 
 	setCurrentPiece();
 	setNextPiece();
-
+	storing = false;
 }
 
 function update(idt) {
-
 	if (playing) {
 
+		//score 표시
 		if (score > vscore) {
-			setVisualScore(vscore + 1)
-			// invalidateScore();
+			vscore = (score - vscore) > 3 ? vscore + 3 : score;
+			setVisualScore(vscore);
 		}
+
+		if (rows > vrows) {
+			setVisualRows(vrows + 1);
+		}
+
+
+
 		handle(actions.shift());
 
 		dt += idt;
@@ -258,26 +278,49 @@ function update(idt) {
 function handle(action) {
 
 	switch (action) {
-		case DIR.LEFT: move(DIR.LEFT); break;
-		case DIR.RIGHT: move(DIR.RIGHT); break;
-		case DIR.UP: rotate(); break;
-		case DIR.DOWN: drop(); break;
-		case DIR.FULLDROP: fullDrop(); break;
+		case KEY.LEFT: move(DIR.LEFT); break;
+		case KEY.RIGHT: move(DIR.RIGHT); break;
+		case KEY.UP: rotate(); break;
+		case KEY.DOWN: drop(); break;
+		case KEY.SPACE: fullDrop(); break;
+		case KEY.SHIFT: storePiece(); break;
 	}
 
+}
+
+function storePiece() {
+
+	if( storing == false ){
+		dt = 0;
+		current.y = 0;
+		if (!stored) {
+			console.log('first storing');
+			// 첫 storing
+			setStoredPiece(current);
+			setCurrentPiece(next);
+			setNextPiece();
+		} else {
+			console.log('get stored block');
+			// 이미 storing -> current와 storing 교환;
+			var tmp = current;
+			randomPosition(stored);
+			setCurrentPiece(stored);
+			setStoredPiece(tmp);
+		}
+		storing = true;
+
+	} else console.log('cannot store');
 }
 
 
 function move(dir) {
 
-
 	var x = current.x;
 	var y = current.y;
-
 	switch (dir) {
 		case DIR.LEFT: x -= 1; break;
 		case DIR.RIGHT: x += 1; break;
-		case DIR.DOWN: y += 1;
+		case DIR.DOWN: y += 1; dt = 0;
 	}
 
 	if (unoccupied(current.type, x, y, current.dir)) {
@@ -293,30 +336,52 @@ function move(dir) {
 function rotate() {
 
 
-	// console.log('rotate()');
+	var x = current.x, y = current.y, nextDir = current.dir;
+	nextDir = (nextDir + 1) > DIR.MAX ? 0 : nextDir + 1;
+
+	var xAdjust = 0;
+	var yAdjust = 0;
+	eachblock(current.type, x, y, nextDir, function (x, y) {
+
+		if (x < 0) {
+			xAdjust = Math.max(xAdjust, -x);
+		} else if (x >= nx) xAdjust = Math.min(xAdjust, nx - 1 - x);
+		if (y >= ny) yAdjust = Math.min(yAdjust, ny - 1 - y);
+	})
+
+	x += xAdjust;
+	y += yAdjust;
+
+	if (unoccupied(current.type, x, y, nextDir)) {
+		current.x = x;
+		current.y = y;
+		current.dir = nextDir;
+		invalidate();
+	}
+
 }
 
 function drop() {
-	console.log(current);
 	if (!move(DIR.DOWN)) {
 
 		setBlock(current);
 		addScore(10);
-		removeLines();
-		setScore();
 
+		removeLines();
 		setCurrentPiece(next);
 		setNextPiece();
-
+		storing = false;
 		// clearActions();
 	}
-
 	if (!unoccupied(current.type, current.x, current.y, current.dir)) lose();
+
+
 }
 
 function fullDrop() {
-	console.log('fullDrop()');
 
+	while (move(DIR.DOWN)) { }
+	drop();
 }
 
 function dropPiece() {
@@ -326,26 +391,44 @@ function dropPiece() {
 
 function removeLines() {
 
+	var count = 0;
 	for (var i = ny - 1; i > 0; i--) {
 		var willRemoved = true;
 		for (var j = 0; j < nx; j++) {
 			if (!getBlock(j, i)) willRemoved = false;
 		}
-		if (willRemoved) removeLine(i);
-		//없앤줄부터 다시검사!
-		i++;
+		if (willRemoved) {
+			removeLine(i);
+			//없앤줄부터 다시검사!
+			i++;
+			count++;
+		}
+	}
+
+	addRows(count);
+	console.log(rows);
+	switch (count) {
+		case 1: addScore(100);
+		case 2: addScore(250);
+		case 3: addScore(400);
+		case 4: addScore(600);
 	}
 }
+
+function lineAnima() {
+
+}
+
 
 function removeLine(n) {
 
 	for (var j = n; j > 0; j--) {
 		for (var i = 0; i < nx; i++) {
 			//윗줄을 아랫줄로
+			blocks[i] = blocks[i] || [];
 			blocks[i][j] = blocks[i][j - 1];
 		}
 	}
-
 }
 
 
@@ -354,22 +437,22 @@ var invalid = {};
 function invalidate() { invalid.court = true };
 function invalidateNext() { invalid.next = true };
 function invalidateScore() { invalid.score = true };
-function invalidateRows() { invalid.row = true };
+function invalidateRows() { invalid.rows = true };
+function invalidateStored() { invalid.stored = true };
 
 
 // RENDERING
 
 function draw() {
-	if(playing){
+	if (playing) {
 
 		//drawCourt, Next, Score
 		ctx.save();
 		ctx.lineWidth = 1;
-	
-		ctx.translate(0.5,0.5);
+
+		// ctx.translate(0.5, 0.5);
 
 		if (invalid.court) {
-			console.log('draw?');
 			drawCourt();
 			invalid.court = false;
 		}
@@ -385,6 +468,10 @@ function draw() {
 			drawRows();
 			invalid.rows = false;
 		}
+		if( invalid.stored ){
+			drawStored();
+			invalid.stored = false;
+		}
 		ctx.restore();
 	}
 
@@ -393,45 +480,49 @@ function draw() {
 function drawCourt() {
 
 
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		// current 랜더링
-		drawPiece(ctx, current);
-	
+	// current 랜더링
+	drawPiece(ctx, current, 0, 0, canvas.width, canvas.height);
 
-
-
-		//court에 존재하는 block 랜더링	
-
-		for (var i = 0; i < nx; i++) {
-			for (var j = 0; j < ny; j++) {
-				if (getBlock(i,j)) drawBlock(ctx, i, j, getBlock(i,j));
-			}
+	//court에 존재하는 block 랜더링	
+	for (var i = 0; i < nx; i++) {
+		for (var j = 0; j < ny; j++) {
+			if (getBlock(i, j)) drawBlock(ctx, i, j, getBlock(i, j).color);
 		}
-
-
-	
-
-
+	}
 }
 
 function drawNext() {
 
+	uctx.clearRect(0, 0, ucanvas.width, ucanvas.height);
+	uctx.save();
+	eachblock(next.type, 1, 1, next.dir, function (x, y) {
+		drawBlock(uctx, x, y, next.type.color);
+	})
 
+	uctx.restore();
+}
+function drawStored(){
+
+	sctx.clearRect(0, 0, scanvas.width, scanvas.height );
+	sctx.save();
+	eachblock( stored.type, 1, 1, stored.dir, function(x, y){
+		drawBlock(sctx, x, y, stored.type.color);
+	})
 }
 
 function drawScore() {
-
-
+	get('score').innerText = vscore;
 }
 
 function drawRows() {
-
-
+	get('rows').innerText = vrows;
 }
 
 //한개 통째로 그림
 function drawPiece(ctx, input) {
-	
+
 	eachblock(input.type, input.x, input.y, input.dir, function (x, y) {
 		drawBlock(ctx, x, y, input.type.color);
 	})
@@ -439,7 +530,6 @@ function drawPiece(ctx, input) {
 }
 //한칸씩 그림
 function drawBlock(ctx, x, y, color) {
-	console.log(color);
 	ctx.fillStyle = color;
 	ctx.fillRect(x * dx, y * dy, dx, dy);
 	ctx.strokeRect(x * dx, y * dy, dx, dy);
